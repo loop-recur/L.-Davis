190b343b56133fabefeadf41e6c4478c104ef8ac
Joe Nelson <cred+github@begriffs.com>
2015-03-03 16:28:32 -0800

    last results of using topicmodel package

--- topics.r
+++ topics.r
@@ -19,13 +19,12 @@ dtm.control <- list(
   weighting = weightTf)
 
 dtm <- DocumentTermMatrix(corp, control = dtm.control)
-inspect(dtm)
 
 burnin <- 500
-iter <- 100
+iter <- 1000
 keep <- 30
 
-k <- 40
+k <- 10
 mods <- LDA(dtm, k, method = "Gibbs",
             control = list(burnin = burnin,
                            iter = iter,
@@ -36,16 +35,18 @@ token.id <- mods@wordassignments$j
 topic.id <- mods@wordassignments$v
 vocab <- mods@terms
 
+# phi <- t(apply(t(topics(mods)) + 0.02, 2, function(x) x/sum(x)))
+# theta <- t(apply(mods@wordassignments + 0.02, 2, function(x) x/sum(x)))
 # dat <- getProbs(token.id, doc.id, topic.id, vocab, K = max(topic.id), sort.topics = "byTerms")
 # phi <- t(dat$phi.hat)
 # theta <- dat$theta.hat[, dat$topic.order]
 
 zson <- createJSON(K = K,
-                   phi = t(phi),
-                   theta = theta,
+                   phi = mods@beta,
+                   theta = mods@gamma,
                    doc.length = sum(dtm),
-                   term.frequency = term.frequency,
-                   topic.proportion = topic.proportion,
+                   term.frequency = as.numeric(table(token.id)),
+                   topic.proportion = as.numeric(table(topic.id)/length(topic.id)),
                    vocab = vocab)
 
 serVis(zson)
